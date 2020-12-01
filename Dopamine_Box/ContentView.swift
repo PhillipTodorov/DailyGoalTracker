98de8f7af58a51c1831deeb5019567ab2ab1e5e8
philliptodorov <ph.todorov@yahoo.co.uk>
2020-12-01 17:57:33 +0000

    Initial Commit

---
+++ Dopamine_Box/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Dopamine_Box
+//
+//  Created by Phillip Todorov on 01/12/2020.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ Dopamine_Box/Dopamine_BoxApp.swift
@@ -0,0 +1,17 @@
+//
+//  Dopamine_BoxApp.swift
+//  Dopamine_Box
+//
+//  Created by Phillip Todorov on 01/12/2020.
+//
+
+import SwiftUI
+
+@main
+struct Dopamine_BoxApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
